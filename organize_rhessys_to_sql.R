dfd7d7031fd1efe66fb1ef9eed3e08f73ad571e9
Rachel Torres <r_torres@ucsb.edu>
2021-07-12 16:26:44 -0700

    updated organizing file into three parts

--- organize_rhessys_to_sql.R
+++ organize_rhessys_to_sql.R
@@ -1,20 +1,29 @@
 # organizing rhessys outputs, from output filters, to be input to future mountain SQL 
 # based on variables given in FB_DB.sql
-setwd("~/fire/")
+#setwd("~/fire/")
+setwd("~/Documents/BigCreek7.2ForExample/out/test/")
 library(tidyverse)
 
+# need to update all for climate scenario with cliamte ID in data frames
+
+#################################################################
 # first table - spatial data point 
+
 sdp_p <- read.csv("spatial_data_point_patchvar.csv")
 sdp_c <- read.csv("spatial_data_point_stratumvar.csv")
+
 sdp_c_patch <- sdp_c %>% 
   group_by(day, month, year, basinID, hillID, zoneID, patchID) %>%
   summarize_at(vars(plantc), list(sum))
+
 all_sdp <- inner_join(sdp_p, sdp_c_patch, 
                       by=c('day', 'month', 'year', 'basinID', 'hillID','zoneID', 'patchID')) %>% 
   mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
   dplyr::select(-basinID, -hillID, -zoneID, -month, -day, -year) 
-# then add climate ID as well 
-# need to get elevation as well - that should be from DEM file but then will add based on patch ID
+
+#need to add column for ID, warmingID
+
+#################################################################
 
 # next table is aggcube
 # need to separate over and unders 
@@ -23,34 +32,40 @@ aggc_p <- read.csv("cube_agg_p.csv")
 aggc_c <- read.csv("cube_agg_c.csv")
 
 # grouping by date only 
-aggc_p_grouped <- aggc_p %>% 
+aggc_p_summed <- aggc_p %>% 
   mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
   group_by(date) %>% 
-  summarize_at(vars(litterc, burn, soilc), list(sum)) # needs more outputs
+  summarize_at(vars(litterc, burn, soilc, evap, canopyevap, streamflow, Qin, Qout), list(sum))
+
+aggc_p_avgd <- aggc_p %>% 
+  mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
+  group_by(date) %>% 
+  summarize_at(vars(depthToGW, family_pct_cover, snowpack, VegAccessWater, rootdepth), list(mean))
+
+aggc_p_grouped <- inner_join(aggc_p_summed, aggc_p_avgd)
 
 # need to double check this - overstory is 1, understory is 2
 aggc_over <- aggc_c %>% dplyr::filter(stratumID==1) %>%
   mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
-  dplyr::select(-CburnUnder, -CmortUnder, -month, -day, -year,
+  dplyr::select(-month, -day, -year,
                 -basinID, -hillID, -zoneID, -patchID, -stratumID) %>% 
   group_by(date) %>% 
   summarize_all(sum) 
 
-
-# need to figure out how to automate this, or make 2:5 values flexible or not date, etc. 
-overnames <- paste(names(aggc_over)[2:7], "Over", sep="")
-colnames(aggc_over)[2:7] <- overnames
+n = ncol(aggc_over)
+overnames <- paste(names(aggc_over)[2:n], "Over", sep="")
+colnames(aggc_over)[2:n] <- overnames
   
 aggc_under <- aggc_c %>% dplyr::filter(stratumID==2) %>%
   mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
-  dplyr::select(-burnCOver, -mortCOver, -month, -day, -year,
+  dplyr::select(-month, -day, -year,
                 -basinID, -hillID, -zoneID, -patchID, -stratumID) %>% 
   group_by(date) %>% 
   summarize_all(sum)
 
-# need to figure out how to automate this, or make 2:5 values flexible or not date, etc. 
-undernames <- paste(names(aggc_under)[2:7], "Under", sep="")
-colnames(aggc_under)[2:7] <- undernames
+n = ncol(aggc_under)
+undernames <- paste(names(aggc_under)[2:n], "Under", sep="")
+colnames(aggc_under)[2:n] <- undernames
 
 cube_agg <- inner_join(aggc_over, aggc_under, by="date")
 cube_agg <- inner_join(cube_agg, aggc_p_grouped, by="date")
@@ -59,19 +74,16 @@ cube_agg <- inner_join(cube_agg, aggc_p_grouped, by="date")
 # [dateIdx] [key],
 # [warmingIdx] [key],
 # [rain] float8 NOT NULL,
-# [streamflow] float 8 NOT NULL,
 # [snowfall] float8 NOT NULL,
-# [snowpack] float8 NOT NULL,
-# [groundevap] float8 NOT NULL,
-# [canopyevap] float8 NOT NULL,
-# [depthToGW] float8 NOT NULL,
-# [vegAccessWater] float8 NOT NULL,
 
-# join all for cube_data_point table 
+# what is agg cube actually is it sum, avg, etc?? 
+
+############################################################
+# cube_data_point table 
 
 cubedp_p <- read.csv("cube_data_point_p.csv")
-cubedp_c <- read.csv("cube_data_point_c.csv")
-cubedp_fire <- read.csv("cube_data_point_c_fire.csv") # don't need this though
+cubedp_c1 <- read.csv("cube_data_point_c_over.csv")
+cubedp_c2 <- read.csv("cube_data_point_c_under.csv")
 
 cube_p_grouped <- cubedp_p %>% 
   mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
@@ -79,30 +91,23 @@ cube_p_grouped <- cubedp_p %>%
   summarize_all(sum) %>%
   dplyr::select(-month, -day, -year, -basinID, -hillID, -zoneID)
 
-# need to change names again and group by patch 
-cube_c_under <- cubedp_c %>% 
-  dplyr::filter(stratumID==2) %>%
+# stratums have already been separated out in the output filter file 
+cube_c_over <- cubedp_c1 %>% 
   mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
   dplyr::select(-month, -day, -year,
-                -basinID, -hillID, -zoneID, -stratumID) %>% # in real version, add these -burnCOver, -mortCOver
+                -basinID, -hillID, -zoneID, -stratumID) %>% 
   group_by(date, patchID) %>% 
   summarize_all(sum)
 
-n_names <- length(names(cube_c_under))
-colnames(cube_c_under)[3:n_names] <- paste(names(cube_c_under)[3:n_names], "Under", sep="")
 
-
-cube_c_over <- cubedp_c %>% 
-  dplyr::filter(stratumID==1) %>%
+cube_c_under <- cubedp_c2 %>% 
   mutate(date = as.Date(paste(year, month, day, sep="/"))) %>%
   dplyr::select(-month, -day, -year,
-                -basinID, -hillID, -zoneID, -stratumID) %>% # in real version, add these -burnCOver, -mortCOver
-  group_by(date) %>% 
+                -basinID, -hillID, -zoneID, - stratumID) %>% 
+  group_by(date, patchID) %>% 
   summarize_all(sum)
 
-n_names <- length(names(cube_c_over))
-colnames(cube_c_over)[3:n_names] <- paste(names(cube_c_over)[3:n_names], "Over", sep="")
-  
+
 overunder <- inner_join(cube_c_under, cube_c_over, by=c("date","patchID"))
 allcube_dp <- right_join(overunder, cube_p_grouped) 
 
@@ -112,32 +117,7 @@ allcube_dp <- right_join(overunder, cube_p_grouped)
 #   [dateIdx] [key],
 #   [cubeIdx] int,
 #   [warmingIdx] [key],
-#   [patchIdx] [key],
-#   [date] nvarchar(255) NOT NULL,
 #   [rain] float8 NOT NULL,
-#   [Qin] float8 NOT NULL,
-#   [Qout] float8 NOT NULL,
-#   [snowfall] float8 NOT NULL,
-#   [snowpack] float8 NOT NULL,
-#   [groundevap] float8 NOT NULL,
-#   [canopyevap] float8 NOT NULL,
-#   [netpsnOver] float8 NOT NULL,
-#   [netpsnUnder] float8 NOT NULL,
-#   [depthToGW] float8 NOT NULL,
-#   [vegAccessWater] float8 NOT NULL,
-#   [heightOver] float8 NOT NULL,
-#   [heightUnder] float8 NOT NULL,
-#   [leafCOver] float8 NOT NULL,
-#   [stemCOver] float8 NOT NULL,
-#   [rootCOver] float8 NOT NULL,
-#   [leafCUnder] float8 NOT NULL,
-#   [stemCUnder] float8 NOT NULL,
-#   [rootCUnder] float8 NOT NULL
-#   [rootdepthCUnder] float8 NOT NULL
-#   [rootdepthCOver] float8 NOT NULL
-#   [coverfract] float8 NOT NULL
-#   [burnCOver] float8 NOT NULL
-#   [burnCUnder] float8 NOT NULL
-#   [mortCUnder] float8 NOT NULL
-#   [mortCOver] float8 NOT NULL
+#   [snowfall] float8 NOT NULL
 # )
+# also ask about percent_cover - might not be family output
